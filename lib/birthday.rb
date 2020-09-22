82cd81b04451d1b57d603286a6d4aa671b84476d
Steven Topazio <sat@northpointprinting.com>
2020-09-22 17:54:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
 
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 
